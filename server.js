c77505ebdebf87804a799163cc7c599b32eac1cf
Gihan_Dayarathna <cagihandayarathna123@gmail.com>
2026-05-12 17:04:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+
 const connectDB = require("./config/db");
 
-
 dotenv.config();
+
 connectDB();
 
 const app = express();
 
-// middleware
 app.use(cors());
 app.use(express.json());
 
-// test route
+app.use("/api/equipment", require("./routes/equipmentRoutes"));
+
 app.get("/", (req, res) => {
-  res.json({ message: "Lab Equipment Borrowing API is working" });
+  res.json({
+    message: "Lab Equipment Borrowing API is working"
+  });
 });
 
 const PORT = process.env.PORT || 5000;
